a488b296077fd782d3d8c0f3ad7942f22c788135
CY Kalu <cy23@tutanota.com>
2021-06-11 21:43:09 +0100

    Refactor

--- src/js/scenes/boot.js
+++ src/js/scenes/boot.js
@@ -12,14 +12,15 @@ export default class extends Phaser.Scene {
   create() {
     const cameraX = this.cameras.main.centerX;
     const cameraY = this.cameras.main.centerY;
+    const cameraHeight = this.cameras.main.height;
 
-    this.add.image(cameraX, cameraY - 100, 'phaserLogo').setScale(0.8);
+    this.add.image(cameraX, cameraY - cameraHeight * 0.15, 'phaserLogo').setScale(0.8);
 
     const gameTitle = this.add.text(0, 0, 'Space Shooter', { fontSize: 32 });
-    gameTitle.setPosition(cameraX - gameTitle.width / 2, cameraY + 100);
+    gameTitle.setPosition(cameraX - gameTitle.width / 2, cameraY + cameraHeight * 0.15);
 
     setTimeout(() => {
-      // this.scene.start('Preload');
+      this.scene.start('Preload');
     }, 3000);
   }
 }
