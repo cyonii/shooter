89fd495d5a9d62220d43eedf11326b3773456061
CY Kalu <cy23@tutanota.com>
2021-06-13 00:45:51 +0100

    Rename variable

--- src/js/scenes/title.js
+++ src/js/scenes/title.js
@@ -10,13 +10,13 @@ export default class extends Phaser.Scene {
   }
 
   create() {
-    const button = this.add
+    const playButton = this.add
       .sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'button')
       .setInteractive();
     const text = this.add.text(0, 0, 'Play', { fontSize: 24, fill: '#fff' });
-    Phaser.Display.Align.In.Center(text, button);
+    Phaser.Display.Align.In.Center(text, playButton);
 
-    button.setInteractive();
-    button.on('pointerup', () => this.scene.start('Game'));
+    playButton.setInteractive();
+    playButton.on('pointerup', () => this.scene.start('Game'));
   }
 }
